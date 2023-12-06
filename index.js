09f184ae12a77d206d91deadd2e5d3b1cd6fb54d
Luisa-Castro <luisa.castroa@hotmail.com>
2023-12-05 20:27:45 -0500

    lee ruta ingresada y la convierte en absoluta

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
-module.exports = () => {
-  // ...
+const { isAbsolutePath, convertAbsolute } = require('./functions.js')
+
+
+function mdLinks(path) {
+  return new Promise((resolve, reject) => {
+    //Lamado de las funciones del archivo functions.js
+    console.log(path);
+    const rutaConvertida = convertAbsolute(path)
+    resolve(rutaConvertida)
+  })
+}
+
+
+module.exports = {
+  mdLinks
 };
